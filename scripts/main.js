20bb6de82b2ed07a3da6021d3feba2b373fcd0af
Beyker Estrada <beyker.chirino.estrada@gmail.com>
2023-02-26 23:49:01 -0300

    Primeros estilos en hero

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+//FUNCION PARA ESCRIBIR DINÁMICAMENTE EL TEXTO DEL TITULO EN EL HERO
+var typed = new Typed('.multiText', {
+    strings: ['Desarrollo electrónico (PCB)', 'Modelado mecánico 3D', 'Animación 3D', 'Ascensores y Elevadores'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1500,
+    loop: true
+});
