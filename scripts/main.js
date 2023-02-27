e218ca7b7b23dc4670b0b6d4e0d7754b98e8b71f
Beyker Estrada <beyker.chirino.estrada@gmail.com>
2023-02-27 16:19:29 -0300

    Cambios de color en el hero

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 //FUNCION PARA ESCRIBIR DINÁMICAMENTE EL TEXTO DEL TITULO EN EL HERO
 var typed = new Typed('.multiText', {
-    strings: ['Desarrollo electrónico (PCB)', 'Modelado mecánico 3D', 'Animación 3D', 'Ascensores y Elevadores'],
+    strings: ['Desarrollo electrónico', 'Modelado mecánico 3D', 'Animación 3D', 'Ascensores y Elevadores'],
     typeSpeed: 50,
     backSpeed: 30,
     backDelay: 1500,
